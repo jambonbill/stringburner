2b70faa02bb0dbb5f169060c9773f766245695fe
orange <jambonbill@gmail.com>
2020-12-05 21:42:53 +0100

    +$gcode=new GC\Gcode("../conf/default.json");

--- test/test.php
+++ test/test.php
@@ -3,9 +3,18 @@
 require __DIR__."/../vendor/autoload.php";
 
 
-$gcode=new PSP\Gcode("../json/test.json");
+$gcode=new GC\Gcode("../conf/default.json");
+$gcode->debug(true);
+$gcode->loadFont("../font/square.json");
 
-echo $gcode->toString();
-//$gcode->toFile("/tmp/test.gcode");
+echo $gcode->conf()."\n";
+//$gcode->text("<JAMBONBILL@GMAIL.COM>\n<JAMBONBILL@GMAIL.COM>\n<JAMBONBILL@GMAIL.COM>");
+
+$text=file_get_contents('poem.txt');
+$gcode->text("@JAMBON");
+
+//echo $gcode->toString();
+
+$gcode->toFile("/tmp/test.gcode");
 
 echo "ok\n";
